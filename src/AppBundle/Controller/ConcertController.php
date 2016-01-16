c17e76fca529ea0b0f67ca0c5910d2e4fb1219c2
ISHIDA Akio <iakio.monospacenet@gmail.com>
2016-01-16 20:18:15 +0900

    ifによる満席表示の切り替え

--- src/AppBundle/Controller/ConcertController.php
+++ src/AppBundle/Controller/ConcertController.php
@@ -15,18 +15,21 @@ class ConcertController extends Controller
         $concertList = [
             [
                 'date' => '2015年5月3日',
-                'place' => '東京文化会館（満席）',
+                'place' => '東京文化会館',
                 'time' => '14:00',
+                'available' => false,
             ],
             [
                 'date' => '2015年7月12日',
                 'place' => '鎌倉芸術館',
                 'time' => '14:00',
+                'available' => true,
             ],
             [
                 'date' => '2015年9月20日',
                 'place' => '横浜みなとみらいホール',
                 'time' => '15:00',
+                'available' => true,
             ],
         ];
 
