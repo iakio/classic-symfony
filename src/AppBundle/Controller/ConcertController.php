584a177fb06f3b9afa483e3aa166c9cd1f623f8d
ISHIDA Akio <iakio.monospacenet@gmail.com>
2016-01-16 20:14:07 +0900

    公演情報の配列化

--- src/AppBundle/Controller/ConcertController.php
+++ src/AppBundle/Controller/ConcertController.php
@@ -12,6 +12,26 @@ class ConcertController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('Concert/index.html.twig');
+        $concertList = [
+            [
+                'date' => '2015年5月3日',
+                'place' => '東京文化会館（満席）',
+                'time' => '14:00',
+            ],
+            [
+                'date' => '2015年7月12日',
+                'place' => '鎌倉芸術館',
+                'time' => '14:00',
+            ],
+            [
+                'date' => '2015年9月20日',
+                'place' => '横浜みなとみらいホール',
+                'time' => '15:00',
+            ],
+        ];
+
+        return $this->render('Concert/index.html.twig', [
+            'concertList' => $concertList
+        ]);
     }
 }
