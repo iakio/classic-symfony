6fb9d347e95d681b3a9749897145f2a680614e63
ISHIDA Akio <iakio.monospacenet@gmail.com>
2016-01-29 01:05:46 +0900

    お問合せ検索機能の作成

--- src/AppBundle/Controller/AdminInquiryListController.php
+++ src/AppBundle/Controller/AdminInquiryListController.php
@@ -5,6 +5,9 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Inquiry;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/admin/inquiry")
@@ -12,16 +15,39 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class AdminInquiryListController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/search")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $form = $this->createSearchForm();
+        $form->handleRequest($request);
+        $keyword = null;
+        if ($form->isValid()) {
+            $keyword = $form->get('search')->getData();
+        }
+
         $em = $this->getDoctrine()->getManager();
         $inquiryRepository = $em->getRepository('AppBundle:Inquiry');
-        $inquiryList = $inquiryRepository->findAll([], ['id' => 'DESC']);
+        $inquiryList = $inquiryRepository->findAllByKeyword($keyword);
 
         return $this->render('Admin/Inquiry/index.html.twig',
-            ['inquiryList' => $inquiryList]
+            [
+                'form' => $form->createView(),
+                'inquiryList' => $inquiryList,
+            ]
         );
+    }
+
+    /**
+     * @return \Symfony\Component\Form\Form
+     */
+    private function createSearchForm()
+    {
+        return $this->createFormBuilder()
+            ->add('search', SearchType::class)
+            ->add('submit', SubmitType::class, [
+                'label' => '検索',
+            ])
+            ->getForm();
     }
 }
